bbcdc01841c506607cd8a1c226e26fbf1a9a2507
kajZdej <an4630@student.uni-lj.si>
2024-11-22 16:52:58 +0100

    dodal sem datoteke za poiskusni pogon

--- Docker/test.sh
+++ Docker/test.sh
@@ -4,9 +4,9 @@ docker pull jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v
 docker run --rm  \
         --ipc=host \
         --memory 256g \
-        --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/test_dataset.json,target=/input_dataset.json \
-        --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/configs_registration.json,target=/configs_registration.json \
-        --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/test_data,target=/input \
-        --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/test_output,target=/output \
+        --mount type=bind,source=/Izziv_AMS/Docker/test_dataset.json,target=/input_dataset.json \
+        --mount type=bind,source=/Izziv_AMS/Docker/configs_registration.json,target=/configs_registration.json \
+        --mount type=bind,source=Izziv_AMS/Docker/input,target=/input \
+        --mount type=bind,source=Izziv_AMS/Docker/onput,target=/output \
         jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v2
 
---
+++ WhatHaveIDone.md
@@ -0,0 +1,5 @@
+ - first tings first. Kloniral sem repozitorij na LST racunalnik
+ - poskus pull jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v2
+ - ni šlo skozi.
+ - dodajam poiskusne datoteke
+ gi
